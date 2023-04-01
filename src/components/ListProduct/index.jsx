673f7850455ceb58c5e6de207a9ed0b28f04ddd3
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2023-04-01 12:49:29 +0700

    update

--- src/components/ListProduct/index.jsx
+++ src/components/ListProduct/index.jsx
@@ -71,7 +71,7 @@ const ListProduct = () => {
             <ul className="inline-flex items-center -space-x-px">
               <li>
                 <a
-                  href="#"
+                  href="javascript:void(0)"
                   className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   <span className="sr-only">Previous</span>
@@ -92,7 +92,7 @@ const ListProduct = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="javascript:void(0)"
                   className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   1
@@ -100,7 +100,7 @@ const ListProduct = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="javascript:void(0)"
                   className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   2
@@ -108,7 +108,7 @@ const ListProduct = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="javascript:void(0)"
                   aria-current="page"
                   className="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
                 >
@@ -117,7 +117,7 @@ const ListProduct = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="javascript:void(0)"
                   className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   4
@@ -125,7 +125,7 @@ const ListProduct = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="javascript:void(0)"
                   className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   5
@@ -133,7 +133,7 @@ const ListProduct = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="javascript:void(0)"
                   className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   <span className="sr-only">Next</span>
