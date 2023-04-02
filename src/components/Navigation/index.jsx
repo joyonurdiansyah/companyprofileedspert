72266654e0d851c6e831f27d7031feaadb9e8f62
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2023-04-02 11:19:52 +0700

    update modul/icon

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Navbar } from 'flowbite-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = () => {
     return (
@@ -13,14 +15,17 @@ const Navigation = () => {
                 <Navbar.Toggle />
                 <Navbar.Collapse>
                     <ul className="items-center justify-between flex flex-col p-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 background-color-header">
-                        <li>
+                        <li className="flex items-center">
                             <span className="text-white">Program</span>
+                            <FontAwesomeIcon icon={faAngleDown} className="text-white ml-2" />
                         </li>
-                        <li >
+                        <li className="flex items-center">
                             <span className="text-white">Bidang Ilmu</span>
+                            <FontAwesomeIcon icon={faAngleDown} className="text-white ml-2" />
                         </li>
-                        <li>
+                        <li className="flex items-center">
                             <span className="text-white">Tentang Edspert</span>
+                            <FontAwesomeIcon icon={faAngleDown} className="text-white ml-2" />
                         </li>
                         <li>
                             <button type="button" className="items-center justify-between text-white focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center" style={{backgroundColor:'#FF6161'}}>Masuk/Daftar</button>
@@ -32,4 +37,4 @@ const Navigation = () => {
     );
 }
 
-export default Navigation
+export default Navigation;
