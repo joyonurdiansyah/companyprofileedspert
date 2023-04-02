4e395be65aeb4fa50721c52baffb1db182358e9a
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2023-04-02 11:06:18 +0700

    update

--- src/containers/Content/index.jsx
+++ src/containers/Content/index.jsx
@@ -1,9 +1,9 @@
-import ListProduct from "../../components/ListProduct"
+import ProductList from "../../components/ProductList"
 
 const Content = () => {
     return (
         <div>
-            <ListProduct />
+            <ProductList />
         </div>
     )
 }
